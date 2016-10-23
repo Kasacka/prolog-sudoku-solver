a215f740d549bff42e893f7e73d6449eb09fea71
Pascal Birchler <hello@pascalbirchler.ch>
2016-10-23 16:29:12 +0200

    Add empty script as the base template

---
+++ sudoku.pl
@@ -0,0 +1,10 @@
+/*
+  @descr This script can be used to solve any NxN sudoku.
+         
+         Uses <a href="http://prologdoc.sourceforge.net/">PrologDoc</a> for documentation.
+
+         Created as part of the course 'Programming Languages and Formal Methods' at
+         University of Applied Sciences Rapperswil.
+  @author Jonas Wälter, Pascal Birchler
+  @date 23/10/2016
+*/
